5ed01ac696f7e83ab847365a92f3df7eb06e7387
rogy10 <igorblazevski10@gmail.com>
2022-06-14 23:33:34 +0200

    Domasna video 1-7

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-console.log('Hey Ninjas')
+// console.log('Hey Ninjas')
+
+
+
+// setTimeout(function(){
+//     console.log('3 second have passed');
+// }, 3000);
+
+
+
+// var time = 0;
+// var timer = setInterval(function(){
+//     time += 2;
+//     console.log(time + ' seconds have passed');
+//     if (time > 5){
+//         clearInterval(timer);
+//     }
+// }, 2000);
+
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+
+// function sayHi(){
+
+//     console.log('HI');
+// }
+// sayHi();
+
+// var sayBye = function(){
+//     console.log('BYE');
+// }
+// sayBye();
+
+
+
 var stuff = require('./stuff');
 
 console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
