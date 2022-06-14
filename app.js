a2989f523e239a61df3639d51306f96888a3bbc4
rogy10 <igorblazevski10@gmail.com>
2022-06-15 00:13:00 +0200

    +8video

--- app.js
+++ app.js
@@ -42,3 +42,28 @@ var stuff = require('./stuff');
 console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
 console.log(stuff.adder(5,6));
 console.log(stuff.adder(stuff.pi, 5));
+
+
+
+var events = require('events');
+var util = require('util');
+
+var Person = function(name){
+    this.name = name;
+}
+
+util.inherits(Person, events.EventEmitter);
+
+var James = new Person('James');
+var Mary = new Person('Mary');
+var Ryu = new Person('Ryu');
+var people = [James, Mary, Ryu];
+
+people.forEach(function(Person){
+    Person.on('speak', function(mssg){
+        console.log(Person.name + ' said ' + mssg);
+    })
+});
+
+James.emit('speak', 'hey dudes');
+Ryu.emit('speak', 'I want a curry');
